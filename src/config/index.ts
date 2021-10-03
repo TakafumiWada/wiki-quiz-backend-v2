20b0ae1c71ab1e7f0335a88a4ac9c052b4890dc3
WadaTakafumi <wdtk2525@gmail.com>
2021-10-03 13:02:28 +0900

    ADD: config を定義

---
+++ src/config/index.ts
@@ -0,0 +1,4 @@
+export const LIMITED_WORD_LENGTH = 18;
+export const SELECTED_WORDS_NUMBER = 9;
+export const MEDIAWIKI_API_URL = "http://ja.wikipedia.org/w/api.php";
+export const LIMIT_ACCESS = 10;
