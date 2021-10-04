95ee4c354f5621369b8ffb4259144cc750c2db44
WadaTakafumi <wdtk2525@gmail.com>
2021-10-05 07:57:07 +0900

    UPDATE: レスポンスをcategoryの上限数に設定

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,5 @@
 export const LIMITED_WORD_LENGTH = 18;
 export const SELECTED_WORDS_NUMBER = 9;
+export const SELECTED_CATEGORIES_NUMBER = 3;
 export const MEDIAWIKI_API_URL = "http://ja.wikipedia.org/w/api.php";
 export const LIMIT_ACCESS = 10;
--- src/interfaces/serializers/QuestionSerializer.ts
+++ src/interfaces/serializers/QuestionSerializer.ts
@@ -1,19 +1,29 @@
 import { IQuestion } from "../../domain/models/Question";
-import { SELECTED_WORDS_NUMBER, LIMITED_WORD_LENGTH } from "../../config";
+import {
+  SELECTED_WORDS_NUMBER,
+  SELECTED_CATEGORIES_NUMBER,
+  LIMITED_WORD_LENGTH,
+} from "../../config";
 
+const randomSelect = (array: Array<string>, num: number): Array<string> => {
+  const newArray: Array<string> = [];
+  let rand = 0;
+  while (newArray.length < num && array.length > 0) {
+    rand = Math.floor(Math.random() * array.length);
+    if (array[rand].length <= LIMITED_WORD_LENGTH) {
+      newArray.push(array[rand]);
+      array.splice(rand, 1);
+    }
+  }
+  return newArray;
+};
 export class QuestionSerializer {
   getQuestionSerialize(quesion: IQuestion) {
-    const array = quesion.words;
-    const limitedArray: Array<string> = [];
-    let rand = 0;
-    while (limitedArray.length < SELECTED_WORDS_NUMBER && array.length > 0) {
-      rand = Math.floor(Math.random() * array.length);
-      if (array[rand].length <= LIMITED_WORD_LENGTH) {
-        limitedArray.push(array[rand]);
-        array.splice(rand, 1);
-      }
-    }
-    quesion.words = limitedArray;
+    quesion.words = randomSelect(quesion.words, SELECTED_WORDS_NUMBER);
+    quesion.categories = randomSelect(
+      quesion.categories,
+      SELECTED_CATEGORIES_NUMBER
+    );
     return quesion;
   }
 }
